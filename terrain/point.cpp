e217bb8278e6790dcdd7df7923f9707d03a37815
abrahim-djordjevic <48631638+abrahim-djordjevic@users.noreply.github.com>
2025-04-16 16:07:06 +0100

    Add files via upload

---
+++ terrain/point.cpp
@@ -0,0 +1,14 @@
+class Point
+{
+    public:
+        float x;
+        float y;
+        float z;
+        Point(float i, float j, float k)
+        {
+            x = i;
+            y = j;
+            z = k;
+        }
+
+};
